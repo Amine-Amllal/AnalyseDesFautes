162349b8ee900f7d9d74da74b2ded83b245f4f7d
Amine Amllal <78987238+Amine-Amllal@users.noreply.github.com>
2025-05-21 11:25:08 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Analyse Des Fautes'
 copyright = '2025, AMLLAL Amine, AKEBLI FatimaEzzahrae, ELHAKIOUI Asmae'
@@ -12,17 +6,26 @@ author = 'AMLLAL Amine, AKEBLI FatimaEzzahrae, ELHAKIOUI Asmae'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'myst_parser',
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+language = 'fr'
+source_suffix = ['.rst', '.md']
 
+todo_include_todos = True
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
